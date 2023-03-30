7608183726129e9840d2c85568cd2e4b9371ae7c
Ayush Agnihotri <ayushagnihotri2025@gmail.com>
2023-03-30 10:50:49 +0530

    ⏰ Time: 39 ms (43.35%), 📦 Space: 14 MB (96.82%) - LeetCode

---
+++ LeetCode/Python3/Medium/22. Generate Parentheses/0022-generate-parentheses.py
@@ -0,0 +1,5 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        if n == 0:
+            return ['']
+        return ['(' + left + ')' + right for i in range(n) for left in self.generateParenthesis(i) for right in self.generateParenthesis(n - i - 1)]
