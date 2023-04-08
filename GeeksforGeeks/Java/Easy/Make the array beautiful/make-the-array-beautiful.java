54a4ddead7168d8d0dfde96e9e16a470f0a5b8e3
Ayush Agnihotri <ayushagnihotri2025@gmail.com>
2023-04-08 19:00:02 +0530

    Added solution 📒- GeeksforGeeks

---
+++ GeeksforGeeks/Java/Easy/Make the array beautiful/make-the-array-beautiful.java
@@ -0,0 +1,69 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class IntArray {
+    public static int[] input(BufferedReader br, int n) throws IOException {
+        String[] s = br.readLine().trim().split(" ");
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) a[i] = Integer.parseInt(s[i]);
+
+        return a;
+    }
+
+    public static void print(int[] a) {
+        for (int e : a) System.out.print(e + " ");
+        System.out.println();
+    }
+
+    public static void print(ArrayList<Integer> a) {
+
+        for (int e : a) System.out.print(e + " ");
+        System.out.println();
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out = new PrintWriter(System.out);
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+
+            int n;
+            n = Integer.parseInt(br.readLine());
+
+            int[] arr = IntArray.input(br, n);
+
+            Solution obj = new Solution();
+            ArrayList<Integer> res = obj.makeBeautiful(arr);
+            for (int e : res) out.print(e + " ");
+            out.println();
+        }
+        out.close();
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    public static ArrayList<Integer> makeBeautiful(int[] arr) {
+        // code here
+        Stack<Integer> s = new Stack<>();
+        for(int i =0; i<arr.length;i++ ){
+            if(s.empty()) {
+                s.push(arr[i]);
+                continue;
+            }
+            if((s.peek()^arr[i])<0) s.pop();
+            else{
+                s.push(arr[i]);
+            }
+        }
+          ArrayList<Integer> list = new  ArrayList<Integer>(s);
+         return list;
+    }
+}
