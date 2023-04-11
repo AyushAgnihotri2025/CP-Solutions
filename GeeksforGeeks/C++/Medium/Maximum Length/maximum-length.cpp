533d6c54c07e2f211f8bd591d561e25d89106341
Ayush Agnihotri <ayushagnihotri2025@gmail.com>
2023-04-11 06:30:57 +0530

    Added solution 📒- GeeksforGeeks

---
+++ GeeksforGeeks/C++/Medium/Maximum Length/maximum-length.cpp
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+public:
+    int solve(int a, int b, int c) {
+        // code here
+        int vec[3] = {a, b, c};
+        sort(vec, vec + 3);
+        if (vec[2] > 2 * (vec[0] + vec[1])){
+            return -1;
+        }
+        return a + b + c;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        int a, b, c;
+        cin >> a >> b >> c;
+
+        Solution ob;
+        int ans = ob.solve(a, b, c);
+     
+
+        cout <<  ans << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
