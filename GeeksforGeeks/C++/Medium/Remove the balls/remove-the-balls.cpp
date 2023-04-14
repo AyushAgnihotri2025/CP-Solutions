dd83885fc8fd6b5cfc31cad9ada547b874a66c93
Ayush Agnihotri <ayushagnihotri2025@gmail.com>
2023-04-14 07:07:34 +0530

    Added solution 📒- GeeksforGeeks

---
+++ GeeksforGeeks/C++/Medium/Remove the balls/remove-the-balls.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    int finLength(int N, vector<int> color, vector<int> radius) {
+        stack <int> ans;
+        for (int i=0; i<N; i++){
+            if (ans.size() > 0){
+                if (color[ans.top()] == color[i] and radius[ans.top()] == radius[i]){
+                    ans.pop();
+                    continue;
+                }
+            }
+            ans.push(i);
+        }
+        return ans.size();
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+
+    int tt;
+    cin >> tt;
+    Solution sol;
+    while (tt--) {
+
+        int n;
+        cin >> n;
+        vector<int> color(n), radius(n);
+        for (int i = 0; i < n; i++) cin >> color[i];
+        for (int i = 0; i < n; i++) cin >> radius[i];
+        int ans = sol.finLength(n, color, radius);
+        cout << ans << "\n";
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
