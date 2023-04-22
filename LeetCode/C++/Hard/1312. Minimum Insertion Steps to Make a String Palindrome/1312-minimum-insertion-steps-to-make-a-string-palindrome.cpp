c1743e830d402efd69f7aededf0a5afed475b284
Ayush Agnihotri <ayushagnihotri2025@gmail.com>
2023-04-22 07:11:15 +0530

    ⏰ Time: 39 ms (84.65%), 📦 Space: 6.7 MB (98.6%) - LeetCode

---
+++ LeetCode/C++/Hard/1312. Minimum Insertion Steps to Make a String Palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int minInsertions(string s) {
+        int n = s.length();
+        vector<int> dp(n, 0);
+
+        for (int i = n - 2; i >= 0; i--) {
+            int prev = 0;
+            for (int j = i + 1; j < n; j++) {
+                int temp = dp[j];
+                if (s[i] == s[j]) {
+                    dp[j] = prev;
+                } else {
+                    dp[j] = 1 + min(dp[j], dp[j-1]);
+                }
+                prev = temp;
+            }
+        }
+
+        return dp[n-1];
+    }
+};
