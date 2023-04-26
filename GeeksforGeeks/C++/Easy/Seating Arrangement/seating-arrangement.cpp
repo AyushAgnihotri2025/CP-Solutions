61e1415c38880310aa22158b48abba10bc0c8b36
Ayush Agnihotri <ayushagnihotri2025@gmail.com>
2023-04-26 05:53:11 +0530

    Added solution 📒- GeeksforGeeks

---
+++ GeeksforGeeks/C++/Easy/Seating Arrangement/seating-arrangement.cpp
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    bool is_possible_to_get_seats(int n, int m, vector<int>& seats){
+        // Write your code here.
+        seats.push_back(0);
+        seats.insert(seats.begin(),0);
+        for (int i=1;i<m+1;i++){
+            if(seats[i-1]==0&&seats[i]==0&&seats[i+1]==0)
+            {
+                n--;
+                seats[i]=1;
+            }
+        }
+        return n<=0?true:false;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int m;
+        cin >> m;
+        vector<int> seats(m);
+        for (int i = 0; i < m; i++) {
+            cin >> seats[i];
+        }
+        Solution obj;
+        if (obj.is_possible_to_get_seats(n, m, seats)) {
+            cout << "Yes" << endl;
+        }
+        else {
+            cout << "No" << endl;
+        }
+    }
+}
+
+// } Driver Code Ends
