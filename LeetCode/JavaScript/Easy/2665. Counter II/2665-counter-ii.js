8a6421ef5b20e8e0820d34200139166c9425c352
Ayush Agnihotri <ayushagnihotri2025@gmail.com>
2023-05-07 16:41:10 +0530

    ⏰ Time: 70 ms (42.55%), 📦 Space: 44.5 MB (81.57%) - LeetCode

---
+++ LeetCode/JavaScript/Easy/2665. Counter II/2665-counter-ii.js
@@ -0,0 +1,32 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let count = init;
+    function increment() {
+        count++;
+        return count;
+    }
+    function decrement() {
+        count--;
+        return count;
+    }
+    function reset() {
+        count = init;
+        return count;
+    }
+    return {
+        increment,
+        decrement,
+        reset
+    };
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
+ 
