240f60a1feae1cb97c7351051dde2c11f67198d7
Ayush Agnihotri <ayushagnihotri2025@gmail.com>
2023-05-11 12:42:17 +0530

    Added solution 📒- GeeksforGeeks

---
+++ GeeksforGeeks/C++/Medium/Non Repeating Numbers/non-repeating-numbers.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+public:
+    vector<int> singleNumber(vector<int> nums) 
+    {
+        // Code here.
+        vector<int> ans;
+        unordered_map<int,int> mp;
+        int n = nums.size();
+        for(int i=0;i<n;++i){
+            mp[nums[i]]++;
+        }
+        
+        for(auto it:mp){
+            if(it.second == 1) ans.push_back(it.first);
+        }
+        
+        sort(ans.begin(),ans.end());
+        
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main(){
+    int T;
+    cin >> T;
+    while(T--)
+    {
+    	int n; 
+    	cin >> n;
+    	vector<int> v(2 * n + 2);
+    	for(int i = 0; i < 2 * n + 2; i++)
+    		cin >> v[i];
+    	Solution ob;
+    	vector<int > ans = ob.singleNumber(v);
+    	for(auto i: ans)
+    		cout << i << " ";
+    	cout << "\n";
+    }
+	return 0;
+}
+// } Driver Code Ends
