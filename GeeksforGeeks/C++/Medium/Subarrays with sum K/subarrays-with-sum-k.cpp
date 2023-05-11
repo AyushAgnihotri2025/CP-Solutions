7f51c38a150891bc8709d0666b06c12d96122cc1
Ayush Agnihotri <ayushagnihotri2025@gmail.com>
2023-05-11 11:42:26 +0530

    Added solution 📒- GeeksforGeeks

---
+++ GeeksforGeeks/C++/Medium/Subarrays with sum K/subarrays-with-sum-k.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    int findSubArraySum(int Arr[], int N, int k)
+    {
+        // code here
+        int ans=0,sum=0;
+        unordered_map<int,int>mp;
+        for(int i=0;i<N;i++){
+            sum+=Arr[i];
+            if(sum==k) ans++;
+            ans+=mp[sum-k];
+            mp[sum]++;
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin >> N;
+        int Arr[N];
+        for(int i=0;i<N;i++)    
+            cin>>Arr[i];
+        int k;
+        cin>>k;
+        Solution obj;
+        cout<<obj.findSubArraySum(Arr, N, k)<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
