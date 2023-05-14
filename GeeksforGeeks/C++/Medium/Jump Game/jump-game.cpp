b4e280b964a6fae18e2de629ecdf159791558939
Ayush Agnihotri <ayushagnihotri2025@gmail.com>
2023-05-14 11:30:16 +0530

    Added solution 📒- GeeksforGeeks

---
+++ GeeksforGeeks/C++/Medium/Jump Game/jump-game.cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+  public:
+    int canReach(int A[], int N) {
+        // code here
+        int dest = N-1 ;
+        for(int i=N-1 ; i>=0 ; i--)
+        {
+            if((A[i]+i) >= dest) dest=i ;
+        }
+        
+        return dest==0 ;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin>>N;
+        
+        int A[N];
+        
+        for(int i=0; i<N; i++)
+            cin>>A[i];
+
+        Solution ob;
+        cout << ob.canReach(A,N) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
