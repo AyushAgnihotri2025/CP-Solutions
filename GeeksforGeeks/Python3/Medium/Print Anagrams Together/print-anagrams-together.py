e2eb0fb37228f931ae222e834772dfdb44f45328
Ayush Agnihotri <ayushagnihotri2025@gmail.com>
2023-05-14 11:23:18 +0530

    Added solution 📒- GeeksforGeeks

---
+++ GeeksforGeeks/Python3/Medium/Print Anagrams Together/print-anagrams-together.py
@@ -0,0 +1,46 @@
+#User function Template for python3
+
+class Solution:
+    def Anagrams(self, words, n):
+        '''
+        words: list of word
+        n:      no of words
+        return : list of group of anagram {list will be sorted in driver code (not word in grp)}
+        '''
+        
+        #code here
+        dic={}
+        lis=[]
+        for i in words:
+            t=str(sorted(i))
+            if t not in dic:
+                dic[t]=[i]
+                lis.append(t)
+            else:
+                dic[t].append(i)
+        ans=[]
+        for i in lis:
+            ans.append(dic[i])
+        return ans
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+#contributed by RavinderSinghPB
+if __name__ =='__main__':
+    t= int(input())
+    for tcs in range(t):
+        n= int(input())
+        words=input().split()
+        
+        ob = Solution()
+        ans = ob.Anagrams(words,n)
+        
+        for grp in sorted(ans):
+            for word in grp:
+                print(word,end=' ')
+            print()
+
+# } Driver Code Ends
