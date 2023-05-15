11dd854e31605788c2118dfddf31be4b0a9243b3
Ayush Agnihotri <ayushagnihotri2025@gmail.com>
2023-05-15 12:55:08 +0530

    ⏰ Time: 73 ms (46.19%), 📦 Space: 16.3 MB (30.25%) - LeetCode

---
+++ LeetCode/Python3/Medium/47. Permutations II/0047-permutations-ii.py
@@ -0,0 +1,17 @@
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        self.getPer(nums, res, 0)
+        return res
+
+    def getPer(self, nums, res, begin):
+        if begin == len(nums):
+            res.append(list(nums))
+        visit = set()
+        for i in range(begin, len(nums)):
+            if nums[i] in visit:
+                continue
+            visit.add(nums[i])
+            nums[begin], nums[i] = nums[i], nums[begin]
+            self.getPer(nums, res, begin+1)
+            nums[begin], nums[i] = nums[i], nums[begin]
