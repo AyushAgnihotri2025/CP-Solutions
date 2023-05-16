fc2778e8772c5effde06f1aa3264dc4224969da6
Ayush Agnihotri <ayushagnihotri2025@gmail.com>
2023-05-16 22:38:01 +0530

    Added solution 📒- GeeksforGeeks

---
+++ GeeksforGeeks/C++/Medium/Reverse a Stack/reverse-a-stack.cpp
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+public:
+    void Reverse(stack<int> &St){
+        vector<int>v;
+        int t=0;
+        while(!St.empty()){
+            int a = St.top();
+            v.push_back(a);
+            St.pop();
+            t++;
+        }
+        for(int i=0;i<t;i++){
+            int b = v[i];
+            St.push(b);
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+
+int main(){
+    int T;
+    cin>>T;
+    while(T--){
+        int N;
+        cin>>N;
+        stack<int> St;
+        for(int i=0;i<N;i++){
+            int x;
+            cin>>x;
+            St.push(x);
+        }
+        Solution ob;
+        ob.Reverse(St);
+        vector<int>res;
+        while(St.size())
+        {
+            res.push_back(St.top());
+            St.pop();
+        }
+        for(int i = res.size()-1;i>=0;i--)
+        {
+            cout<<res[i]<<" ";
+        }
+        
+        cout<<endl;
+    }
+}
+// } Driver Code Ends
