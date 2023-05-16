00aecff26af66da9ff4c2215220b1828278b018d
Ayush Agnihotri <ayushagnihotri2025@gmail.com>
2023-05-16 22:44:47 +0530

    Added solution 📒- GeeksforGeeks

---
+++ GeeksforGeeks/C++/Medium/Sum of bit differences/sum-of-bit-differences.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:
+	
+	long long sumBitDifferences(int arr[], int n) {
+	    // code here
+	    long long sum=0;
+	    for(long long i=0;i<32;i++){
+	        long long ones=0,twos=0;
+	        for(int j=0;j<n;j++){
+	          if((arr[j]&(1<<i))){
+	               ones++;
+	          } 
+	          else{
+	              twos++;
+	          }
+	        }
+	        sum+=(ones*twos);
+	    }
+	    return 2*sum;
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, i;
+        cin >> n;
+        int arr[n];
+        for (i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.sumBitDifferences(arr, n);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
