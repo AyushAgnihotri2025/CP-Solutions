7b973dc8064a449b7072f66340217ba88d20f03b
Ayush Agnihotri <ayushagnihotri2025@gmail.com>
2023-05-24 11:43:48 +0530

    ⏰ Time: 46 ms (32.76%), 📦 Space: 16.2 MB (42.1%) - LeetCode

---
+++ LeetCode/Python3/Medium/91. Decode Ways/0091-decode-ways.py
@@ -0,0 +1,15 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        if s[0] == '0':
+            return 0
+        n = len(s)
+        dp = [0 for _ in range(n + 1)]
+        dp[0] = 1
+        for i in range(1, n + 1):
+            if s[i - 1] != '0':
+                dp[i] = dp[i - 1]
+            if i >= 2:
+                x = int(s[i - 2: i])
+                if 10 <= x <= 26:
+                    dp[i] += dp[i - 2]
+        return dp[n]
