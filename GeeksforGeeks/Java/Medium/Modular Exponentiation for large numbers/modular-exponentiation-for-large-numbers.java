b6b85bdd9c077f8baf878aca12807ea398872c1f
Ayush Agnihotri <ayushagnihotri2025@gmail.com>
2023-05-24 06:22:26 +0530

    Added solution 📒- GeeksforGeeks

---
+++ GeeksforGeeks/Java/Medium/Modular Exponentiation for large numbers/modular-exponentiation-for-large-numbers.java
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            String s = br.readLine();
+            String[] S = s.split(" ");
+            long x = Long.parseLong(S[0]);
+            long n = Long.parseLong(S[1]);
+            long m = Long.parseLong(S[2]);
+            Solution ob = new Solution();
+            long ans = ob.PowMod(x, n, m);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public long PowMod(long x, long n, long m)
+    {
+        // Code here
+        long ans=1;
+        x = x%m;
+        if(x==0)
+            return 0;
+        while(n>0) {
+            if((n&1)!=0)
+                ans = (ans*x)%m;
+            x = (x*x)%m;
+            n = n>>1;
+        }
+        return ans;
+    }
+}
