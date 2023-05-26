383de4b9d26eebfd1320784a75233b17dae4a578
Ayush Agnihotri <ayushagnihotri2025@gmail.com>
2023-05-26 08:09:56 +0530

    ⏰ Time: 51 ms (39.72%), 📦 Space: 16.3 MB (50.74%) - LeetCode

---
+++ LeetCode/Python3/Easy/88. Merge Sorted Array/0088-merge-sorted-array.py
@@ -0,0 +1,8 @@
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        for i in range(m,m+n):
+            nums1[i]=nums2[i-m]
+        nums1.sort()
