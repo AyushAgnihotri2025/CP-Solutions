29414466bc7c98528265be642d5007c534414d3f
Ayush Agnihotri <ayushagnihotri2025@gmail.com>
2023-05-29 06:48:32 +0530

    Added solution 📒- GeeksforGeeks

---
+++ GeeksforGeeks/C++/Medium/Bleak Numbers/bleak-numbers.cpp
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+public:
+	int is_bleak(int n)
+	{
+	    // Code here.
+	    for(int i=0; i<n; i++){
+	       int count = __builtin_popcount(i);
+	       if((i + count) == n)
+	            return 0;
+	   }
+	   return 1;
+	}
+};
+
+
+//{ Driver Code Starts.
+int main(){
+    int T;
+    cin >> T;
+    while(T--)
+    {
+    	int n; 
+    	cin >> n;
+    	Solution ob;
+    	int ans = ob.is_bleak(n);
+    	cout << ans << "\n";
+    }
+	return 0;
+}
+
+// } Driver Code Ends
