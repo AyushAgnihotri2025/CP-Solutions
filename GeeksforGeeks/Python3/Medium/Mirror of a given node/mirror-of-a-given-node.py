42eeb33de35eb5b7abbecf96c41c908341b4458c
Ayush Agnihotri <ayushagnihotri2025@gmail.com>
2023-05-29 07:13:08 +0530

    Added solution 📒- GeeksforGeeks

---
+++ GeeksforGeeks/Python3/Medium/Mirror of a given node/mirror-of-a-given-node.py
@@ -0,0 +1,106 @@
+#User function Template for python3
+
+
+'''
+class Node:
+    def __init__(self,val):
+        self.data=val
+        self.left=None
+        self.right=None
+'''
+class Solution:
+    def solve(self,a,b,target):
+        if not a or not b:
+            return -1
+        self.dic[a.data]=b.data
+        self.dic[b.data]=a.data
+        self.solve(a.left,b.right,target)
+        self.solve(a.right,b.left,target)
+
+    def findMirror(self,root, target):
+        #code here
+        self.dic={}
+        self.dic[root.data]=root.data
+        self.solve(root.left,root.right,target)
+        if target not in  self.dic:
+            return -1
+        return self.dic[target]
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+from collections import deque
+class Node:
+    def __init__(self,val):
+        self.data=val
+        self.left=None
+        self.right=None
+        
+def buildTree(s):
+    #Corner Case
+    if(len(s)==0 or s[0]=="N"):           
+        return None
+        
+    # Creating list of strings from input 
+    # string after spliting by space
+    ip=list(map(str,s.split()))
+    
+    # Create the root of the tree
+    root=Node(int(ip[0]))                     
+    size=0
+    q=deque()
+    
+    # Push the root to the queue
+    q.append(root)                            
+    size=size+1 
+    
+    # Starting from the second element
+    i=1                                       
+    while(size>0 and i<len(ip)):
+        # Get and remove the front of the queue
+        currNode=q[0]
+        q.popleft()
+        size=size-1
+        
+        # Get the current node's value from the string
+        currVal=ip[i]
+        
+        # If the left child is not null
+        if(currVal!="N"):
+            
+            # Create the left child for the current node
+            currNode.left=Node(int(currVal))
+            
+            # Push it to the queue
+            q.append(currNode.left)
+            size=size+1
+        # For the right child
+        i=i+1
+        if(i>=len(ip)):
+            break
+        currVal=ip[i]
+        
+        # If the right child is not null
+        if(currVal!="N"):
+            
+            # Create the right child for the current node
+            currNode.right=Node(int(currVal))
+            
+            # Push it to the queue
+            q.append(currNode.right)
+            size=size+1
+        i=i+1
+    return root
+    
+if __name__ == '__main__':
+    test_cases = int(input())
+    for cases in range(test_cases):
+        s=input()
+        root=buildTree(s)
+        target = int(input());
+        ob = Solution()
+        res = ob.findMirror(root,target)
+        print(res)
+# } Driver Code Ends
