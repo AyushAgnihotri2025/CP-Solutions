5e826c4d7feb8ef87a1b011f5ee7594f0fd8226c
Ayush Agnihotri <ayushagnihotri2025@gmail.com>
2023-05-30 09:17:59 +0530

    Added solution 📒- GeeksforGeeks

---
+++ GeeksforGeeks/C++/Medium/Possible Path/possible-path.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution {
+public:
+	int isPossible(vector<vector<int>>paths){
+	    // Code here
+        int n = paths.size();
+        int m = paths[0].size();
+        for(int i=0;i<n;i++){
+            int count = 0;
+            for(int j=0;j<m;j++){
+               if(paths[i][j]==1)
+                    count++;
+            }
+            if(count&1) return 0;
+        }
+        return 1;
+	}
+
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n;
+		cin >> n;
+		vector<vector<int>>paths(n, vector<int>(n, 0));
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < n; j++)
+				cin >> paths[i][j];
+		}
+		Solution obj;
+		int ans = obj.isPossible(paths);
+		cout << ans <<"\n";
+	}
+	return 0;
+}
+// } Driver Code Ends
