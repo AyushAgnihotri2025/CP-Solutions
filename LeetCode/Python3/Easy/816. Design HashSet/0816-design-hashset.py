c60833dc0ff508976fcd38e146876011b4dad380
Ayush Agnihotri <ayushagnihotri2025@gmail.com>
2023-05-30 08:45:58 +0530

    ⏰ Time: 173 ms (61.83%), 📦 Space: 21.5 MB (50.10%) - LeetCode

---
+++ LeetCode/Python3/Easy/816. Design HashSet/0816-design-hashset.py
@@ -0,0 +1,27 @@
+class MyHashSet:
+    def __init__(self):
+        self.size = 1000
+        self.buckets = [[] for _ in range(self.size)]
+
+    def add(self, key: int) -> None:
+        index = self._hash(key)
+        if key not in self.buckets[index]:
+            self.buckets[index].append(key)
+
+    def remove(self, key: int) -> None:
+        index = self._hash(key)
+        if key in self.buckets[index]:
+            self.buckets[index].remove(key)
+
+    def contains(self, key: int) -> bool:
+        index = self._hash(key)
+        return key in self.buckets[index]
+
+    def _hash(self, key: int) -> int:
+        return key % self.size
+
+# Your MyHashSet object will be instantiated and called as such:
+# obj = MyHashSet()
+# obj.add(key)
+# obj.remove(key)
+# param_3 = obj.contains(key)
