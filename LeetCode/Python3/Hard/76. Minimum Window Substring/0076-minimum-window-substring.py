053518df56886aed8200128bdebd519e3deabb6f
Ayush Agnihotri <ayushagnihotri2025@gmail.com>
2023-06-02 08:13:47 +0530

    ⏰ Time: 87 ms (99.50%), 📦 Space: 17.1 MB (60.6%) - LeetCode

---
+++ LeetCode/Python3/Hard/76. Minimum Window Substring/0076-minimum-window-substring.py
@@ -0,0 +1,27 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        if len(s) < len(t):
+            return ""
+        needstr = collections.defaultdict(int)
+        for ch in t:
+            needstr[ch] += 1
+        needcnt = len(t)
+        res = (0, float('inf'))
+        start = 0
+        for end, ch in enumerate(s):
+            if needstr[ch] > 0:
+                needcnt -= 1
+            needstr[ch] -= 1
+            if needcnt == 0:
+                while True:
+                    tmp = s[start]
+                    if needstr[tmp] == 0:
+                        break
+                    needstr[tmp] += 1
+                    start += 1
+                if end - start < res[1] - res[0]:
+                    res = (start, end)
+                needstr[s[start]] += 1
+                needcnt += 1
+                start += 1
+        return '' if res[1] > len(s) else s[res[0]:res[1]+1]
