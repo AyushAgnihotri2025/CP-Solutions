b6ae9631348b201eea06be2aebe3340f5b5b4fbd
Ayush Agnihotri <ayushagnihotri2025@gmail.com>
2023-06-02 08:11:24 +0530

    ⏰ Time: 50 ms (33.78%), 📦 Space: 16.4 MB (13.15%) - LeetCode

---
+++ LeetCode/Python3/Medium/75. Sort Colors/0075-sort-colors.py
@@ -0,0 +1,15 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        left = i = 0
+        right = len(nums) - 1
+        while i <= right :
+            while(nums[i] == 2 and i < right):
+                nums[i], nums[right] = nums[right], nums[i]
+                right -= 1
+            while(nums[i] == 0 and i > left):
+                nums[i], nums[left] = nums[left], nums[i]
+                left +=1
+            i += 1
