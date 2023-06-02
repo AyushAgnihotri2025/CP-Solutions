c676dc43573c64d9f9194a07e4ea69cd99b45bfb
Ayush Agnihotri <ayushagnihotri2025@gmail.com>
2023-06-02 08:10:14 +0530

    ⏰ Time: 54 ms (56.83%), 📦 Space: 17 MB (10.62%) - LeetCode

---
+++ LeetCode/Python3/Medium/74. Search a 2D Matrix/0074-search-a-2d-matrix.py
@@ -0,0 +1,6 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        for row in matrix:
+           if target in row:
+               return True
+        return False
