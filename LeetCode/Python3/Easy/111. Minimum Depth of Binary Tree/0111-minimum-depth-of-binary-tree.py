722d13a1a3a13fbd0cfd612ec4137adb9d11d900
Ayush Agnihotri <ayushagnihotri2025@gmail.com>
2023-06-03 20:42:35 +0530

    ⏰ Time: 629 ms (24.81%), 📦 Space: 57.3 MB (8.84%) - LeetCode

---
+++ LeetCode/Python3/Easy/111. Minimum Depth of Binary Tree/0111-minimum-depth-of-binary-tree.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def minDepth(self, root: Optional[TreeNode]) -> int:
+        if root is None:
+            return 0
+        leftDepth = self.minDepth(root.left)
+        rightDepth = self.minDepth(root.right)
+        if root.left is None and root.right is None:
+            return 1
+        if root.left is None:
+            return 1 + rightDepth
+        if root.right is None:
+            return 1 + leftDepth
+        return min(leftDepth, rightDepth) + 1
