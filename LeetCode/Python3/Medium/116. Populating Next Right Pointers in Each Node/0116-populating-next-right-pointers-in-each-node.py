4d1ece0d82e12039bf18f6c1eaf55f7b04bbb602
Ayush Agnihotri <ayushagnihotri2025@gmail.com>
2023-06-03 20:50:20 +0530

    ⏰ Time: 76 ms (39.46%), 📦 Space: 18.3 MB (6.48%) - LeetCode

---
+++ LeetCode/Python3/Medium/116. Populating Next Right Pointers in Each Node/0116-populating-next-right-pointers-in-each-node.py
@@ -0,0 +1,23 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+class Solution:
+    def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
+        if not root:
+            return None
+        q = deque([root])
+        while q:
+            rightNode = None
+            for _ in range(len(q)):
+                cur = q.popleft()
+                cur.next, rightNode = rightNode, cur
+                if cur.right:
+                    q.extend([cur.right, cur.left])
+        return root
