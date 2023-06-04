b3527c7eb80c9844c20d8cf19e0fb55dbebabe89
Ayush Agnihotri <ayushagnihotri2025@gmail.com>
2023-06-04 21:50:23 +0530

    ⏰ Time: 56 ms (70.42%), 📦 Space: 42.2 MB (26.96%) - LeetCode

---
+++ LeetCode/JavaScript/Easy/2796. Allow One Function Call/2796-allow-one-function-call.js
@@ -0,0 +1,21 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+var once = function(fn) {
+    let times=0;
+    return function(...args){
+           times+=1;     
+           if(times==1)
+               return fn(...args);
+           return undefined;
+    }
+};
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
