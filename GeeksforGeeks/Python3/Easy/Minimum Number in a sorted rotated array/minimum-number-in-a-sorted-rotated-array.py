d2e00c3e812267f2260354d0471edd73f89c2773
Ayush Agnihotri <ayushagnihotri2025@gmail.com>
2023-06-05 07:55:14 +0530

    Added solution 📒- GeeksforGeeks

---
+++ GeeksforGeeks/Python3/Easy/Minimum Number in a sorted rotated array/minimum-number-in-a-sorted-rotated-array.py
@@ -0,0 +1,40 @@
+#User function Template for python3
+class Solution:
+    #Function to find the minimum element in sorted and rotated array.
+    def minNumber(self, arr,low,high):
+        #Your code here
+        if arr[low]<=arr[high]:
+            return arr[low]
+        while(low<=high):
+            mid=(low+high)//2
+            if mid<high and arr[mid+1]<arr[mid]:
+                return arr[mid+1]
+            if mid>low and arr[mid]<arr[mid-1]:
+                return arr[mid]
+            if arr[high]>arr[mid]:
+                high=mid-1
+            else:
+                low=mid+1
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import math
+
+
+def main():
+        T=int(input())
+        while(T>0):
+            
+            N=int(input())
+
+            A=[int(x) for x in input().strip().split()]
+            obj = Solution()
+            print(obj.minNumber(A,0,N-1))
+            
+            T-=1
+
+
+if __name__ == "__main__":
+    main()
+# } Driver Code Ends
