73f374558e5ed06e8facc408f1080d02f202cc5e
Ayush Agnihotri <ayushagnihotri2025@gmail.com>
2023-06-07 08:21:21 +0530

    Added solution 📒- GeeksforGeeks

---
+++ GeeksforGeeks/Python3/Easy/Sum of Series/sum-of-series.py
@@ -0,0 +1,21 @@
+#User function Template for python3
+class Solution:
+	def seriesSum(self, n):
+	    # code here
+        return int(n*(n+1)/2)
+
+#{ 
+ # Driver Code Starts
+                               #Initial Template for Python 3
+
+
+# Driver code 
+if __name__ == "__main__": 		
+    tc=int(input())
+    while tc > 0:
+        n=int(input())
+        ob = Solution()
+        ans = ob.seriesSum(n)
+        print(ans)
+        tc=tc-1
+# } Driver Code Ends
