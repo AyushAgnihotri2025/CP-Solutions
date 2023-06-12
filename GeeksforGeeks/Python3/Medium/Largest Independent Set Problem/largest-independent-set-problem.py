57371b6a88194a4a8cb07b78a53c36d4f95c9c99
Ayush Agnihotri <ayushagnihotri2025@gmail.com>
2023-06-12 07:28:59 +0530

    Added solution 📒- GeeksforGeeks

---
+++ GeeksforGeeks/Python3/Medium/Largest Independent Set Problem/largest-independent-set-problem.py
@@ -0,0 +1,105 @@
+#User function Template for python3
+
+cache = {}
+
+def LISS(root):
+    #code here
+    if id(root) in cache:
+        return cache[root]
+    if not root:
+        return 0
+    l = LISS(root.left)
+    r = LISS(root.right)
+    ans = 0
+    ans = max(ans, l + r)
+    liss = 1
+    if root.left:
+        liss += LISS(root.left.left)
+        liss += LISS(root.left.right)
+    if root.right:
+        liss += LISS(root.right.left)
+        liss += LISS(root.right.right)
+    ans = max(ans, liss)
+    cache[root] = ans
+    return ans
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+from collections import deque
+# Tree Node
+class Node:
+    def __init__(self, val):
+        self.right = None
+        self.data = val
+        self.left = None
+        
+        
+# Tree Class
+
+# Function to Build Tree   
+def buildTree(s):
+    #Corner Case
+    if(len(s)==0 or s[0]=="N"):           
+        return None
+        
+    # Creating list of strings from input 
+    # string after spliting by space
+    ip=list(map(str,s.split()))
+    
+    # Create the root of the tree
+    root=Node(int(ip[0]))                     
+    size=0
+    q=deque()
+    
+    # Push the root to the queue
+    q.append(root)                            
+    size=size+1 
+    
+    # Starting from the second element
+    i=1                                       
+    while(size>0 and i<len(ip)):
+        # Get and remove the front of the queue
+        currNode=q[0]
+        q.popleft()
+        size=size-1
+        
+        # Get the current node's value from the string
+        currVal=ip[i]
+        
+        # If the left child is not null
+        if(currVal!="N"):
+            
+            # Create the left child for the current node
+            currNode.left=Node(int(currVal))
+            
+            # Push it to the queue
+            q.append(currNode.left)
+            size=size+1
+        # For the right child
+        i=i+1
+        if(i>=len(ip)):
+            break
+        currVal=ip[i]
+        
+        # If the right child is not null
+        if(currVal!="N"):
+            
+            # Create the right child for the current node
+            currNode.right=Node(int(currVal))
+            
+            # Push it to the queue
+            q.append(currNode.right)
+            size=size+1
+        i=i+1
+    return root
+    
+    
+if __name__ == '__main__':
+    test_cases = int(input())
+    for cases in range(test_cases):
+        s=input()
+        root=buildTree(s)
+        print(LISS(root))
+# } Driver Code Ends
