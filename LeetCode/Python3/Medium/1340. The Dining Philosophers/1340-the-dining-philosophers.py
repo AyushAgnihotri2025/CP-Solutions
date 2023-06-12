e9c73d6a6bf464addfb2c6674cb8e630f9dd8b5f
Ayush Agnihotri <ayushagnihotri2025@gmail.com>
2023-06-12 08:30:02 +0530

    ⏰ Time: 148 ms (9%), 📦 Space: 17.4 MB (25.59%) - LeetCode

---
+++ LeetCode/Python3/Medium/1340. The Dining Philosophers/1340-the-dining-philosophers.py
@@ -0,0 +1,14 @@
+from threading import Semaphore
+
+class DiningPhilosophers:
+    def __init__(self):
+        self.sizelock = Semaphore(4)
+        self.locks = [Semaphore(1) for _ in range(5)]
+
+    # call the functions directly to execute, for example, eat()
+    def wantsToEat(self, index, *actions):
+        left, right = index, (index - 1) % 5
+        with self.sizelock:
+            with self.locks[left], self.locks[right]:
+                for action in actions:
+                    action()
