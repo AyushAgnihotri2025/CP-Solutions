067fc382d1f6ad30a4314b75b75b0778cfc0d6c0
Ayush Agnihotri <ayushagnihotri2025@gmail.com>
2023-06-12 07:58:47 +0530

    ⏰ Time: 52 ms (26.46%), 📦 Space: 16.4 MB (11.1%) - LeetCode

---
+++ LeetCode/Python3/Easy/257. Binary Tree Paths/0257-binary-tree-paths.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
+        if not root:
+            return []
+        if not root.left and not root.right:
+            return [str(root.val)]
+        paths = []
+        if root.left:
+            paths += [str(root.val) + '->' + path for path in self.binaryTreePaths(root.left)]
+        if root.right:
+            paths += [str(root.val) + '->' + path for path in self.binaryTreePaths(root.right)]
+        return paths
