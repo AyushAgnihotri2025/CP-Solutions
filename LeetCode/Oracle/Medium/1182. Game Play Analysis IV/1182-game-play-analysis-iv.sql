adf94bd8f6625c578514af3a9005114f89c8296b
Ayush Agnihotri <ayushagnihotri2025@gmail.com>
2023-06-12 08:48:38 +0530

    ⏰ Time: 1228 ms (7.59%), 📦 Space: undefined (undefined) - LeetCode

---
+++ LeetCode/Oracle/Medium/1182. Game Play Analysis IV/1182-game-play-analysis-iv.sql
@@ -0,0 +1,2 @@
+/* Write your PL/SQL query statement below */
+SELECT ROUND(COUNT(a2.player_id) / COUNT(a.player_id), 2) AS fraction FROM activity a LEFT JOIN activity a2 ON a2.player_id = a.player_id AND a2.event_date = a.event_date + 1 WHERE (a.player_id, a.event_date) IN (SELECT player_id, MIN(event_date) FROM activity GROUP BY player_id);
