5d30eb15541f6a81023b9f960da506598915a169
Ayush Agnihotri <ayushagnihotri2025@gmail.com>
2023-06-12 07:56:08 +0530

    ⏰ Time: 44 ms (46.58%), 📦 Space: 16.4 MB (13.5%) - LeetCode

---
+++ LeetCode/Python3/Easy/168. Excel Sheet Column Title/0168-excel-sheet-column-title.py
@@ -0,0 +1,7 @@
+class Solution:
+    def convertToTitle(self, columnNumber: int) -> str:
+        output = ""
+        while columnNumber > 0:
+            output = chr(ord('A') + (columnNumber - 1) % 26) + output
+            columnNumber = (columnNumber - 1) // 26
+        return output
