5e2969cd6ecc6506a4dec4754dc31b08d9a3c7e9
Ayush Agnihotri <ayushagnihotri2025@gmail.com>
2023-06-14 09:20:29 +0530

    ⏰ Time: 2438 ms (5.1%), 📦 Space: undefined (undefined) - LeetCode

---
+++ LeetCode/MySQL/Easy/1541. Top Travellers/1541-top-travellers.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+select name, ifnull(sum(distance), 0) as travelled_distance from Users left join Rides on Users.id = Rides.user_id group by user_id order by travelled_distance desc, name asc;
