4959e9c048b717b5c15ae69e2fdfd90ab99f52dc
Ayush Agnihotri <ayushagnihotri2025@gmail.com>
2023-06-28 08:55:23 +0530

    Added solution 📒- GeeksforGeeks

---
+++ GeeksforGeeks/Python3/Easy/Delete middle element of a stack/delete-middle-element-of-a-stack.py
@@ -0,0 +1,33 @@
+#User function Template for python3
+
+class Solution:
+    #Function to delete middle element of a stack.
+    def deleteMid(self, s, sizeOfStack):
+        # code here
+        s.pop((0+sizeOfStack-1)//2)
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+import sys
+sys.setrecursionlimit(10**8)
+
+def main():
+    t=int(input())
+    while(t>0):
+        sizeOfStack=int(input())
+        myStack=[int(x) for x in input().strip().split()]
+        ob = Solution()
+        ob.deleteMid(myStack,sizeOfStack)
+        while(len(myStack)>0):
+            print(myStack[-1],end=" ")
+            myStack.pop()
+        print()
+        t-=1
+
+
+if __name__=="__main__":
+    main()
+    
+    
+# } Driver Code Ends
