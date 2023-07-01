95c337551bd550c22789c7783fe4562e91a9a189
Ayush Agnihotri <ayushagnihotri2025@gmail.com>
2023-07-01 20:41:45 +0530

    ⏰ Time: 9458 ms (5.98%), 📦 Space: 16.3 MB (51.49%) - LeetCode

---
+++ LeetCode/Python3/Medium/1418. Fair Distribution of Cookies/1418-fair-distribution-of-cookies.py
@@ -0,0 +1,19 @@
+class Solution:
+    def distributeCookies(self, cookies: List[int], k: int) -> int:
+        min_unfairness = float('inf')
+        distribution = [0] * k
+
+        def backtrack(i):
+            nonlocal min_unfairness, distribution
+            if i == len(cookies):
+                min_unfairness = min(min_unfairness, max(distribution))
+                return
+            if min_unfairness <= max(distribution):
+                return
+            for j in range(k):
+                distribution[j] += cookies[i]
+                backtrack(i + 1)
+                distribution[j] -= cookies[i]
+
+        backtrack(0)
+        return min_unfairness
