d4ed9ff08d36db193ee0c0ca159dd27240dca7b5
Ayush Agnihotri <ayushagnihotri2025@gmail.com>
2023-07-13 22:31:33 +0530

    Added solution 📒- GeeksforGeeks

---
+++ GeeksforGeeks/Python3/Easy/Unique Number of Occurrences/unique-number-of-occurrences.py
@@ -0,0 +1,39 @@
+from typing import List
+from collections import Counter
+
+class Solution:
+    def isFrequencyUnique(self, n : int, arr : List[int]) -> bool:
+        # code here
+        return True if len(Counter(arr))==len(set(Counter(arr).values())) else False
+
+#{ 
+ # Driver Code Starts
+
+class IntArray:
+    def __init__(self) -> None:
+        pass
+    def Input(self,n):
+        arr=[int(i) for i in input().strip().split()]#array input
+        return arr
+    def Print(self,arr):
+        for i in arr:
+            print(i,end=" ")
+        print()
+
+
+if __name__=="__main__":
+    t = int(input())
+    for _ in range(t):
+        
+        n = int(input())
+        
+        
+        arr=IntArray().Input(n)
+        
+        obj = Solution()
+        res = obj.isFrequencyUnique(n, arr)
+        
+        result_val = 1 if res else 0
+        print(result_val)
+
+# } Driver Code Ends
