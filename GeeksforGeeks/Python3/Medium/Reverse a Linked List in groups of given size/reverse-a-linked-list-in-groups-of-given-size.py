ded76a5c334c03e040d57ffbd7550e26d6585adf
Ayush Agnihotri <ayushagnihotri2025@gmail.com>
2023-07-21 14:53:41 +0530

    Added solution 📒- GeeksforGeeks

---
+++ GeeksforGeeks/Python3/Medium/Reverse a Linked List in groups of given size/reverse-a-linked-list-in-groups-of-given-size.py
@@ -0,0 +1,75 @@
+"""Return reference of new head of the reverse linked list
+  The input list will have at least one element
+  Node is defined as
+
+class Node:
+    def __init__(self, data):
+		self.data = data
+		self.next = None
+  This is method only submission.
+  You only need to complete the method.
+"""
+class Solution:
+    def reverse(self,head, k):
+        # Code here
+        if not head:
+            return None
+        prev = None
+        temp = None
+        curr = head
+        count = 0
+        while curr and count<k:
+            temp = curr.next
+            curr.next = prev
+            prev,curr = curr,temp
+            count+=1
+        if temp:
+            head.next = self.reverse(temp,k) 
+        return prev
+
+#{ 
+ # Driver Code Starts
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        # self.tail
+
+    def push(self, new_data):
+        new_node = Node(new_data)
+        new_node.next = self.head
+        self.head = new_node
+
+    def printList(self):
+        temp = self.head
+        while (temp):
+            print(temp.data, end=" ")
+            # arr.append(str(temp.data))
+            temp = temp.next
+        print()
+
+if __name__ == '__main__':
+    t = int(input())
+    while (t > 0):
+        llist = LinkedList()
+        n = input()
+        values = list(map(int, input().split()))
+        for i in reversed(values):
+            llist.push(i)
+        k = int(input())
+        new_head = LinkedList()
+        ob=Solution()
+        new_head = ob.reverse(llist.head, k)
+        llist.head = new_head
+        llist.printList()
+        t -= 1
+
+
+
+
+# } Driver Code Ends
